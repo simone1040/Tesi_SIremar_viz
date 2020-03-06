3be52d7102577f8a96e1328918a11f1e355192ea
simone <simone1040@hotmail.it>
2020-03-06 19:51:12 +0100

    refactoring del codice e utilizzo del dato max carico preso dal database

--- controllers/CaricoManager.py
+++ controllers/CaricoManager.py
@@ -5,81 +5,128 @@ from utils.MyLogger import writeLog
 import pandas as pd
 
 
-
-#Funzione che viene chiamata quando si clicca il tasto di generazione del grafico, che viene salvato sull'assets
-def image_statistics_filtered(filter):
-    list_of_ship_name = []
-    dataframe_to_plot = DATAFRAME_APPLICATION["dataframe_prenotazioni"]
-    #PRENDO IL DATABASE DELLE ROUTE CAPPELLI COSI DA POTER FORMARE IL NUOVO DATABASE DA CUI SI EFFETTUA IL PLOT
-
+def get_filtered_dataframe(dataframe, filter):
     for key, values in filter.items():
         if values != "":
             if key == "ship":
                 writeLog(levelLog.INFO,"CaricoManager","Sto filtrando per le seguenti navi --> {}".format(filter["ship"]))
-                dataframe_to_plot = dataframe_to_plot[dataframe_to_plot["ship_code"].isin(filter["ship"])]
+                dataframe = dataframe[dataframe["ship_code"].isin(filter["ship"])]
             if key == "departure_port_code":
                 port_name = from_port_code_get_name(filter["departure_port_code"])
-                dataframe_to_plot = dataframe_to_plot[dataframe_to_plot["departure_port_name"] == port_name]
+                dataframe = dataframe[dataframe["departure_port_name"] == port_name]
             if key == "arrival_port_code":
                 port_name = from_port_code_get_name(filter["arrival_port_code"])
-                dataframe_to_plot = dataframe_to_plot[dataframe_to_plot["arrival_port_name"] == port_name]
+                dataframe = dataframe[dataframe["arrival_port_name"] == port_name]
             if key == "booking_ticket_departure_timestamp":
                 booking_ticket_departure_timestamp = filter["booking_ticket_departure_timestamp"]
-                dataframe_to_plot = dataframe_to_plot[dataframe_to_plot["booking_ticket_departure_timestamp"] >= booking_ticket_departure_timestamp]
+                dataframe = dataframe[dataframe["booking_ticket_departure_timestamp"] >= booking_ticket_departure_timestamp]
             if key == "booking_ticket_arrival_timestamp":
                 booking_ticket_arrival_timestamp = filter["booking_ticket_arrival_timestamp"]
-                dataframe_to_plot = dataframe_to_plot[dataframe_to_plot["booking_ticket_departure_timestamp"] <= booking_ticket_arrival_timestamp]
+                dataframe = dataframe[dataframe["booking_ticket_departure_timestamp"] <= booking_ticket_arrival_timestamp]
+    return dataframe
+
+#Funzione che dalla data, mi da una rapprentazione che non tiene conto dell'anno
+def get_month_day_from_date(data):
+    splitted_data = data.split("-")
+    return splitted_data[1] + "-" + splitted_data[2]
+
+#Funzione che aggiunge la colonna departure timestamp da DAta-ora in mese-giorno
+def transform_departure_timestamp(dataframe_to_plot):
+    dataframe_to_plot["booking_ticket_departure_timestamp"] = dataframe_to_plot[
+        "booking_ticket_departure_timestamp"].apply(lambda x: x.split(" ")[0])
+
+    dataframe_to_plot["departure_month_day"] = dataframe_to_plot["booking_ticket_departure_timestamp"]\
+        .apply(lambda x: get_month_day_from_date(x))
+    return dataframe_to_plot
+
+def build_image_to_show(dataframe_to_plot, filter):
+    dataframe_mq_occupati = DATAFRAME_APPLICATION["dataframe_max_mq_occupati"]
+    #INIZIALIZZO LA LISTA DELLE NAVI
+    list_of_ship_name = []
+    # PRENDO DAI FILTRI LA DATA DI PARTENZA E ARRIVO PER MOSTRARLA
+    booking_ticket_arrival_timestamp = filter["booking_ticket_arrival_timestamp"]
+    booking_ticket_departure_timestamp = filter["booking_ticket_departure_timestamp"]
+    fig = plt.figure(figsize=(IMAGE_WIDTH / IMAGE_INFO["dpi_monitor"], IMAGE_HEIGHT / IMAGE_INFO["dpi_monitor"]),
+                     dpi=IMAGE_INFO["dpi_monitor"])
+    dataframe_to_plot = transform_departure_timestamp(dataframe_to_plot)
+    for values in dataframe_to_plot.ship_code.unique():
+        # INIZIO DATAFRAME PER IL LIMITE CHE COSI PRENDE L'INTERO GRAFICO
+        mq_max = dataframe_mq_occupati[dataframe_mq_occupati["ship_code"] == values][
+            "metri_garage_navi_spazio_totale"].iloc[0]
+        dataframe_lim = pd.DataFrame(dataframe_to_plot.booking_ticket_departure_timestamp.unique(),
+                                     columns=['booking_ticket_departure_timestamp'])
+        dataframe_lim["metri_garage_navi_spazio_totale"] = mq_max
+        # FINE
+        # INIZIO PRENDO IL NOME DELLA NAVE E LO APPENDO DUE VOLTE PERCHE LO USO DUE VOLTE NEL PLOT
+        ship_name = get_ship_name(values)
+        list_of_ship_name.append(ship_name)
+        list_of_ship_name.append(ship_name)
+        # FINE
+        # INIZIO PLOT DEL LIMITE E DEI PUNTI
+        plt.plot(dataframe_lim["booking_ticket_departure_timestamp"],
+                 dataframe_lim["metri_garage_navi_spazio_totale"], label=ship_name)
+
+        plt.plot(dataframe_to_plot[dataframe_to_plot["ship_code"] == values]["booking_ticket_departure_timestamp"],
+                 dataframe_to_plot[dataframe_to_plot["ship_code"] == values]["tot_mq_occupati"], 'o', label=ship_name)
+        # FINE
+    plt.title(
+        "Mq occupati da imbarchi dal " + booking_ticket_departure_timestamp + " al " + booking_ticket_arrival_timestamp,
+        loc="center")
+    plt.xlabel('Data di partenza')
+    plt.legend()
+    return fig, list_of_ship_name
+
+
+def get_label_month(ax):
+    current_month = -1
+    labels = []
+    for n, label in enumerate(ax.xaxis.get_ticklabels()):
+        text = label.get_text()
+        exploded_text = text.split("-")
+        month = int(exploded_text[1].lstrip("0"))
+        if current_month == -1:
+            current_month = month
+            labels.append(text)
+        elif current_month < month:
+            current_month = month
+            labels.append(text)
+        else:
+            if((current_month == 12) and (month == 1)):
+                current_month = month
+                labels.append(text)
+    return labels
+
+def set_fig_properties(labels):
+    plt.legend()
+    plt.xticks(labels, rotation=30)
+    plt.ylabel('Mq')
+    plt.grid()
+
+def color_same_ship_with_same_color(ax, list_of_ship_name):
+    for i, p in enumerate(ax.get_lines()):  # this is the loop to change Labels and colors
+        if p.get_label() in list_of_ship_name[:i]:  # check for Name already exists
+            idx = list_of_ship_name.index(p.get_label())  # find ist index
+            p.set_c(ax.get_lines()[idx].get_c())  # set color
+            p.set_label('_' + p.get_label())  # hide label in auto-legend
+
+#Funzione che viene chiamata quando si clicca il tasto di generazione del grafico, che viene salvato sull'assets
+def image_statistics_filtered(filter):
+    # PRENDO IL DATABASE DELLE PRENOTAZIONI
+    dataframe_to_plot = DATAFRAME_APPLICATION["dataframe_prenotazioni"]
+
+    #EFFETTUO IL FILTRAGGIO PER I FILTRI SELEZIONATI
+    dataframe_to_plot = get_filtered_dataframe(dataframe_to_plot, filter)
+
     #Ordino le righe per il departure timestamp
     dataframe_to_plot = dataframe_to_plot.sort_values(by = ["booking_ticket_departure_timestamp"])
+
     if not dataframe_to_plot.empty:
-        fig = plt.figure(figsize=(IMAGE_WIDTH/IMAGE_INFO["dpi_monitor"], IMAGE_HEIGHT/IMAGE_INFO["dpi_monitor"]), dpi=IMAGE_INFO["dpi_monitor"])
-        dataframe_to_plot["booking_ticket_departure_timestamp"] = dataframe_to_plot["booking_ticket_departure_timestamp"].apply(lambda x: x.split(" ")[0])
-        for values in dataframe_to_plot.ship_code.unique():
-            #INIZIO DATAFRAME PER IL LIMITE CHE COSI PRENDE L'INTERO GRAFICO
-            dataframe_lim = pd.DataFrame(dataframe_to_plot.booking_ticket_departure_timestamp.unique(),columns =['booking_ticket_departure_timestamp'])
-            dataframe_lim["metri_garage_navi_spazio_totale"] = dataframe_to_plot[dataframe_to_plot["ship_code"] == values]["metri_garage_navi_spazio_totale"].iloc[0]
-            #FINE
-            #INIZIO PRENDO IL NOME DELLA NAVE E LO APPENDO DUE VOLTE PERCHE LO USO DUE VOLTE NEL PLOT
-            ship_name = get_ship_name(values)
-            list_of_ship_name.append(ship_name)
-            list_of_ship_name.append(ship_name)
-            #FINE
-            #INIZIO PLOT DEL LIMITE E DEI PUNTI
-            plt.plot(dataframe_lim["booking_ticket_departure_timestamp"],
-                     dataframe_lim["metri_garage_navi_spazio_totale"],label=ship_name)
-
-            plt.plot(dataframe_to_plot[dataframe_to_plot["ship_code"] == values]["booking_ticket_departure_timestamp"],
-                 dataframe_to_plot[dataframe_to_plot["ship_code"] == values]["tot_mq_occupati"], 'o',label=ship_name)
-            #FINE
-
-        plt.title("Mq occupati da imbarchi dal " + booking_ticket_departure_timestamp + " al " + booking_ticket_arrival_timestamp, loc="center")
-        plt.xlabel('Data di partenza')
-        plt.legend()
+        fig, list_of_ship_name = build_image_to_show(dataframe_to_plot, filter)
         ax = fig.gca()  # get the current axis
-        for i, p in enumerate(ax.get_lines()):  # this is the loop to change Labels and colors
-            if p.get_label() in list_of_ship_name[:i]:  # check for Name already exists
-                idx = list_of_ship_name.index(p.get_label())  # find ist index
-                p.set_c(ax.get_lines()[idx].get_c())  # set color
-                p.set_label('_' + p.get_label())  # hide label in auto-legend
+        color_same_ship_with_same_color(ax, list_of_ship_name)
         fig.canvas.draw()
-        label_month = ""
-        labels = []
-        for n, label in enumerate(ax.xaxis.get_ticklabels()):
-            text = label.get_text()
-            exploded_text = text.split("-")
-            month = str(exploded_text[1])
-            if month == "12":
-                month = "00"
-            if label_month == "":
-                label_month = month
-                labels.append(text)
-            elif label_month < month:
-                label_month = month
-                labels.append(text)
-        plt.legend()
-        plt.xticks(labels,rotation=30)
-        plt.ylabel('Mq')
-        plt.grid()
+        labels = get_label_month(ax)
+        set_fig_properties(labels)
         return fig
     else:
         return None
